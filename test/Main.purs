8b4f09f06c1b51f8c867d040f759fd4a29099b37
Rajiv Abraham <rajiv.abraham@gmail.com>
2016-12-15 18:52:49 -0500

    test file

--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,27 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Aff          (later')
+import Test.Spec                  (describe, it)
+import Test.Spec.Runner           (run)
+import Test.Spec.Assertions       (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Node.Process (PROCESS)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+
+
+main :: forall t1. Eff ( process :: PROCESS , console :: CONSOLE | t1) Unit
+main = run [consoleReporter] do
+  describe "purescript-spec" do
+    describe "Attributes" do
+      it "awesome" do
+        let isAwesome = true
+        isAwesome `shouldEqual` false
+    describe "Features" do
+      it "runs in NodeJS" $ pure unit
+      it "runs in the browser" $ pure unit
+      it "supports async specs" do
+        res <- later' 100 $ pure "Alligator"
+        res `shouldEqual` "Alligator"
+      it "is PureScript 0.10.1 compatible" $ pure unit
